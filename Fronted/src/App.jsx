db30325dcecd42f6f9935281f69bb248adbbeaa0
RAVI8054 <ravi.jsd@hotmail.com>
2025-07-23 10:38:17 +0530

    Navbar completed with react icone and react-avtar

--- Fronted/src/App.jsx
+++ Fronted/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from './Componets/Header.jsx/Navbar'
+import Sidebar from './Componets/Header.jsx/Sidebar'
 
 function App() {
   return (
-    <div>
-     <h1>app</h1>
+    <div className=''>
+     <Navbar/>
+     <Sidebar/>
     </div>
   )
 }
