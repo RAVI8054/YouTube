6dca793b827465ea1c2582a0de013039af6468b5
RAVI8054 <ravi.jsd@hotmail.com>
2025-08-24 10:32:14 +0530

    deploying chang

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 import mongoose from 'mongoose'
 import cookieParser from 'cookie-parser';
-import videoRoutes from  './routes/video.Route.js'
+import videoRoutes from './routes/video.Route.js'
 import channelRoutes from './routes/channel.Route.js'
 import cors from "cors"
 import commentRouter from "./routes/comments.Route.js";
@@ -22,7 +22,7 @@ mongoose.set('strictQuery', false);
 const app = express();
 app.use(cors({
     origin: "http://localhost:5173",
-    credentials:true
+    credentials: true
 }))
 app.use(express.json());
 app.use(cookieParser());
@@ -33,6 +33,8 @@ commentRouter(app);
 userRouter(app);
 channelRoutes(app)
 
-let port=8080
-app.listen(port,()=>(console.log(`backed started at ${port}`)
-))
+
+const PORT = process.env.PORT || 8080;  // Use Render's port OR fallback to 8080 for local
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Backend started at ${PORT}`);
+});
